ef4066b94c37da5fd6b7bf17d405a90c38b1d992
Gary Hughes <garyedwardhughes@gmail.com>
2020-04-27 09:49:17 +1000

    Strip non ascii characters.

--- lexicographer/sanitise.py
+++ lexicographer/sanitise.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 
 def sanitise(string):
-    return string.replace('\n', '').replace('"', '\'')
+    return string.replace('\n', '').replace('"', '\'').encode("ascii", errors="ignore").decode()
