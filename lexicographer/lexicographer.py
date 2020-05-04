0298761c926aded8b8197d6d7fabaf161177838a
Gary Hughes <garyedwardhughes@gmail.com>
2020-05-04 16:06:54 +1000

    Add a proper argument parser.

--- lexicographer/lexicographer.py
+++ lexicographer/lexicographer.py
@@ -2,26 +2,22 @@
 
 import os
 import sys
+import argparse
 from orchestra import *
 from version_fields_generator import *
 from version_messages_generator import *
 
-def usage():
-    print('{} NAMESPACE PREFIX PATH'.format(os.path.basename(sys.argv[0])))
-
-
 if __name__ == '__main__':
-    
-    if len(sys.argv) != 4:
-        usage()
-        sys.exit(1)
 
-    # TODO - validate these
-    namespace = sys.argv[1]
-    prefix = sys.argv[2] 
-    filename = sys.argv[3]
-    orchestration = Orchestration(filename)
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--namespace', required=True, help='The namespace to generate code in')
+    parser.add_argument('--prefix', required=True, help='The prefix for the generated filenames')
+    parser.add_argument('--orchestration', required=True, help='THe orchestration filename to generate code for')
 
-    generate_version_fields(namespace, prefix, orchestration)
-    generate_version_messages(namespace, prefix, orchestration)
+    args = parser.parse_args()
+
+    orchestration = Orchestration(args.orchestration)
+
+    generate_version_fields(args.namespace, args.prefix, orchestration)
+    generate_version_messages(args.namespace, args.prefix, orchestration)
 
