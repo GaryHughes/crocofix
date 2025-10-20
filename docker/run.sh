98306c405dee133d8933a476108643e05a9502ba
Gary Hughes <garyedwardhughes@gmail.com>
2025-10-21 09:10:18 +1100

    Set the working directory when running the container.

--- docker/run.sh
+++ docker/run.sh
@@ -4,4 +4,4 @@ source "config.sh"
 
 source_path="$(realpath ..)"
 
-docker run -it --rm -v ${source_path}:/crocofix ${build_image_name}
+docker run -it --rm -v ${source_path}:/crocofix -w /crocofix ${build_image_name}
