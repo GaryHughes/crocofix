57fc8419d80b400842c0bdff66c5fbfa5fa83f2e
Gary Hughes <garyedwardhughes@gmail.com>
2025-11-17 21:24:57 +1100

    build and test separately.

--- build_clang.sh
+++ build_clang.sh
@@ -7,10 +7,11 @@ build_dir=build-$build_type
 
 if [ ! -d ${build_dir} ]; then
 	mkdir ${build_dir}
-	cd ${build_dir}
+	pushd ${build_dir}
 	cmake -G Ninja -DCMAKE_BUILD_TYPE=${build_type} -DCMAKE_TOOLCHAIN_FILE=../clang_toolchain.cmake ..
 else
-	cd ${build_dir}
+	pushd ${build_dir}
 fi
 
 ninja "$@" -j8
+popd
