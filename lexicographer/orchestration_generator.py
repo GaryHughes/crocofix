441c5000a374407db18e57928de0f5883893906b
Gary Hughes <garyedwardhughes@gmail.com>
2025-11-17 11:22:03 +1100

    Fix the orchestration generator.

--- lexicographer/orchestration_generator.py
+++ lexicographer/orchestration_generator.py
@@ -12,26 +12,29 @@ def generate_orchestration(namespace, module, partition, prefix, orchestration):
 export module {}:{}_orchestration;
 
 import :orchestration;
+import :{}_messages;
+import :{}_fields;
 
 export namespace {}
 {{
-'''.format(module, partition, namespace)
+'''.format(module, partition, partition, partition, namespace)
         file.write(header)
 
         body = \
 '''
 class orchestration : public crocofix::dictionary::orchestration
-{
+{{
 public:
     
     orchestration()
-    : crocofix::dictionary::orchestration(messages(), fields())
-    {
-    }
+    : crocofix::dictionary::orchestration({}::messages(), {}::fields())
+    {{
+    }}
 
-};
+}};
 
-'''
+'''.format(namespace, namespace)
+        
         file.write(body)
 
         trailer = \
