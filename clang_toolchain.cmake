96adff88497828792e25170615cf562d915fc2bd
Gary Hughes <garyedwardhughes@gmail.com>
2025-11-18 11:29:19 +1100

    Remove -Wno-unneeded-internal-declaration

--- clang_toolchain.cmake
+++ clang_toolchain.cmake
@@ -14,7 +14,6 @@ set(CMAKE_CXX_FLAGS_MINSIZEREL     "-Os -DNDEBUG")
 set(CMAKE_CXX_FLAGS_RELEASE        "-O3 -DNDEBUG")
 set(CMAKE_CXX_FLAGS_RELWITHDEBINFO "-O2 -g")
 
-add_definitions(-Wno-unneeded-internal-declaration)
 add_definitions(-Wno-unused-private-field)
 add_definitions(-Wno-pessimizing-move)
 add_definitions(-Wno-mismatched-tags)
