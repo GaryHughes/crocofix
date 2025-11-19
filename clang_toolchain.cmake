0ac5e4950b00d9af6047728bbeba5690e6af0497
Gary Hughes <garyedwardhughes@gmail.com>
2025-11-19 12:02:53 +1100

    Remove -Wno-delete-non-abstract-non-virtual-dtor

--- clang_toolchain.cmake
+++ clang_toolchain.cmake
@@ -10,11 +10,10 @@ set(CMAKE_C_FLAGS_RELWITHDEBINFO "-O2 -g")
 set(CMAKE_CXX_COMPILER             "${CLANG_ROOT}/bin/clang++")
 set(CMAKE_CXX_FLAGS                "-Wall -ftemplate-backtrace-limit=0 -fPIC")
 set(CMAKE_CXX_FLAGS_DEBUG          "-g")
-set(CMAKE_CXX_FLAGS_MINSIZEREL     "-Os -DNDEBUG")
+set(CMAKE_CXX_FLAGS_MINSIZEREL     "-Os -DNDEBUG") 
 set(CMAKE_CXX_FLAGS_RELEASE        "-O3 -DNDEBUG")
 set(CMAKE_CXX_FLAGS_RELWITHDEBINFO "-O2 -g")
 
-add_definitions(-Wno-delete-non-abstract-non-virtual-dtor)
 add_definitions(-D _WCHAR_T=int)
 
 include($ENV{VCPKG_ROOT}/scripts/buildsystems/vcpkg.cmake)
